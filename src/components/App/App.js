98110ed6dc97646106243ae15a3bc7b1dadd5e3f
Slayer50169 <130939549+Slayer50169@users.noreply.github.com>
2023-11-28 08:46:57 -0500

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import Nav from "../Nav/Nav.js";
 import Routes from '../Routes/Routes';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import React from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax';
